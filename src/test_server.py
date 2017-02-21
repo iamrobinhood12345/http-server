b8b6ec1c62b73e87d74b7f573b8e34dd19d56f0d
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-02-21 15:07:40 -0800

    formatting.

--- src/test_server.py
+++ src/test_server.py
@@ -7,14 +7,14 @@ import pytest
 def test_response_ok():
     """Test to see if valid client request will return a 200 OK message."""
     from client import client
-    response = ("HTTP/1.1 200 OK\r\n" +
-        "Server: Teddy Bear\r\n" +
-        "Content-Type: text/html\r\n" +
-        "Connection: Closed\r\n\r\n" +
-        "<html>\r\n" +
-        "<body>\r\n" +
-        "<h1>Hello, World!</h1>\r\n" +
-        "</body>\r\n" +
+    response = ("HTTP/1.1 200 OK\r\n"
+        "Server: Teddy Bear\r\n"
+        "Content-Type: text/html\r\n"
+        "Connection: Closed\r\n\r\n"
+        "<html>\r\n"
+        "<body>\r\n"
+        "<h1>Hello, World!</h1>\r\n"
+        "</body>\r\n"
         "</html>")
     assert client("GET the bl®rg woot") == response
 
@@ -22,13 +22,13 @@ def test_response_ok():
 def test_response_failed():
     """Test to see if invalid client request will return a 500 Error message."""
     from client import client
-    response = ("HTTP/1.1 500 Internal Server Error\r\n" +
-        "Server: Teddy Bear\r\n" +
-        "Content-Type: text/html\r\n" +
-        "Connection: Closed\r\n\r\n" +
-        "<html>\r\n" +
-        "<body>\r\n" +
-        "<h1>Internal Server Error</h1>\r\n" +
-        "</body>\r\n" +
+    response = ("HTTP/1.1 500 Internal Server Error\r\n"
+        "Server: Teddy Bear\r\n"
+        "Content-Type: text/html\r\n"
+        "Connection: Closed\r\n\r\n"
+        "<html>\r\n"
+        "<body>\r\n"
+        "<h1>Internal Server Error</h1>\r\n"
+        "</body>\r\n"
         "</html>")
     assert client("Get the bl®rg woot") == response
