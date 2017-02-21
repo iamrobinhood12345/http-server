f4bf2da35e99a3e08b8a219b0f26db15597a58ac
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-02-21 15:07:18 -0800

    2 compatible.

--- src/server.py
+++ src/server.py
@@ -1,3 +1,4 @@
+
 """Server programmed to recieve messages from client and return http response."""
 
 
@@ -6,156 +7,67 @@ import sys
 
 
 def server():
-    """Recieve a message from the client and sends a response back."""
+    """Recieve a message from the client and echos it back."""
     while True:
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
-        address = ("127.0.0.1", 7000)
+        address = ("127.0.0.1", 6015)
         server_socket.bind(address)
         server_socket.listen(1)
         conn, addr = server_socket.accept()
         buffer_length = 8
-        req = buffer_request(buffer_length, conn)
-        print(req)
-        conn.sendall(parse_request(req).encode('utf-8'))
+        message_complete = False
+        request = u""
+        while not message_complete:
+            addition = conn.recv(buffer_length)
+            request += addition.decode('utf-8')
+            if len(addition) < buffer_length:
+                break
+        print(request)
+        if method_validation(request):
+            message = response_ok()
+            if sys.version_info[0] == 2:
+                message = message.decode('utf-8')
+            conn.sendall(message.encode('utf-8'))
+        else:
+            message = response_error()
+            if sys.version_info[0] == 2:
+                message = message.decode('utf-8')
+            conn.sendall(message.encode('utf-8'))
         conn.close()
-
-
-def buffer_request(buffer_length, conn):
-    """Return a decoded client request."""
-    req = ""
-    while True:
-        addition = conn.recv(buffer_length).decode('utf-8')
-        req += addition
-        if len(addition) < buffer_length:
-            break
-    return req
-
-
-def parse_request(request):
-    """Return a server response based on client request formatting."""
-    if not method_validation(request):
-        return response_error("method")
-    elif not version_validation(request):
-        return response_error("version")
-    elif not host_validation(request):
-        return response_error("host")
-    elif not format_validation(request):
-        return response_error("format")
-    return response_error("OK")
+        server_socket.close()
 
 
 def method_validation(request):
-    """Verify method formatting and type."""
-    if request[0:4] != "GET ":
-        return False
-    return True
+    """Verify method."""
+    return request[:4] == u"GET "
 
 
-def version_validation(request):
-    """Verify version formatting and type."""
-    for ind in range(0, len(request)):
-        if request[ind:ind + 9] == " HTTP/1.1":
-            return True
-    return False
+def response_ok():
+    """Return 200 OK response."""
+    response = ("HTTP/1.1 200 OK\r\n"
+        "Server: Teddy Bear\r\n"
+        "Content-Type: text/html\r\n"
+        "Connection: Closed\r\n\r\n"
+        "<html>\r\n"
+        "<body>\r\n"
+        "<h1>Hello, World!</h1>\r\n"
+        "</body>\r\n"
+        "</html>")
+    return response
 
 
-def host_validation(request):
-    """Verify host formatting and type."""
-    for ind in range(0, len(request)):
-        if request[ind:ind + 8] == "\r\nHost: ":
-            return True
-    return False
-
-
-def format_validation(request):
-    """Verify format formatting and type."""
-    val_count = 0
-    for ind in range(0, len(request)):
-        if val_count == 0 or val_count == 1:
-            if request[ind] == " ":
-                val_count += 1
-            elif request[ind:ind + 1] == "\r\n":
-                return False
-        elif val_count == 2:
-            if request[ind:ind + 2] == "\r\n":
-                val_count += 1
-            elif request[ind] == " ":
-                return False
-        elif val_count % 2 == 1:
-            if request[ind] == ":":
-                val_count += 1
-            elif request[ind] == " ":
-                return False
-        else:
-            if request[ind] == "\r\n":
-                val_count += 1
-    return True
-
-
-def response_error(key):
-    """Return evaluated request resopnse, either OK or a specific response error."""
-    response_dict = {
-        "OK": ("HTTP/1.1 200 OK\r\n" +
-                    "Date: Mon, 23 May 2005 22:38:34 GMT\r\n" +
-                    "Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n" +
-                    "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n" +
-                    "Etag: '3f80f-1b6-3e1cb03b'\r\n" +
-                    "Accept-Ranges:  none\r\n" +
-                    "Content-Length: 438\r\n" +
-                    "Connection: close\r\n" +
-                    "Content-Type: text/html; charset=UTF-8\r\n" +
-                    "\r\n" +
-                    "<438 bytes of content>"),
-        "method": ("HTTP/1.1 405 Method Not Allowed\r\n" +
-                    "Date: Mon, 23 May 2005 22:38:34 GMT\r\n" +
-                    "Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n" +
-                    "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n" +
-                    "Etag: '3f80f-1b6-3e1cb03b'\r\n" +
-                    "Accept-Ranges:  none\r\n" +
-                    "Content-Length: 438\r\n" +
-                    "Connection: close\r\n" +
-                    "Content-Type: text/html; charset=UTF-8\r\n" +
-                    "\r\n" +
-                    "<438 bytes of content>"),
-        "version": ("HTTP/1.1 403 Forbidden\r\n" +
-                    "Date: Mon, 23 May 2005 22:38:34 GMT\r\n" +
-                    "Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n" +
-                    "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n" +
-                    "Etag: '3f80f-1b6-3e1cb03b'\r\n" +
-                    "Accept-Ranges:  none\r\n" +
-                    "Content-Length: 438\r\n" +
-                    "Connection: close\r\n" +
-                    "Content-Type: text/html; charset=UTF-8\r\n" +
-                    "\r\n" +
-                    "<438 bytes of content>"),
-        "host": ("HTTP/1.1 417 Expectation Failed\r\n" +
-                    "Date: Mon, 23 May 2005 22:38:34 GMT\r\n" +
-                    "Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n" +
-                    "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n" +
-                    "Etag: '3f80f-1b6-3e1cb03b'\r\n" +
-                    "Accept-Ranges:  none\r\n" +
-                    "Content-Length: 438\r\n" +
-                    "Connection: close\r\n" +
-                    "Content-Type: text/html; charset=UTF-8\r\n" +
-                    "\r\n" +
-                    "<438 bytes of content>"),
-        "format": ("HTTP/1.1 400 Bad Request\r\n" +
-                    "Date: Mon, 23 May 2005 22:38:34 GMT\r\n" +
-                    "Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n" +
-                    "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n" +
-                    "Etag: '3f80f-1b6-3e1cb03b'\r\n" +
-                    "Accept-Ranges:  none\r\n" +
-                    "Content-Length: 438\r\n" +
-                    "Connection: close\r\n" +
-                    "Content-Type: text/html; charset=UTF-8\r\n" +
-                    "\r\n" +
-                    "<438 bytes of content>"),
-    }
-    for each in response_dict:
-        if key == each:
-            return response_dict[key]
-    return response_dict["format"]
-
+def response_error():
+    """Return 500 Internal Server Error response."""
+    response = ("HTTP/1.1 500 Internal Server Error\r\n"
+        "Server: Teddy Bear\r\n"
+        "Content-Type: text/html\r\n"
+        "Connection: Closed\r\n\r\n"
+        "<html>\r\n"
+        "<body>\r\n"
+        "<h1>Internal Server Error</h1>\r\n"
+        "</body>\r\n"
+        "</html>")
+    return response
 
 if __name__ == '__main__':
     """Run server from command line."""
--- src/test_server.py
+++ src/test_server.py
@@ -4,179 +4,31 @@
 import pytest
 
 
-REQUESTS = [
-    [
-        "OK",
-        ("GET /teddy/bear.html HTTP/1.1\r\n" +
-        "Date: Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
-        "Server: Teddy Bear\r\n" +
-        "Host:  \r\n")
-    ],
-    [
-        "method",
-        ("PUT /teddy/bear.html HTTP/1.1\r\n" +
-        "Date: Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
-        "Server: Teddy Bear\r\n" +
-        "Host:  \r\n")
-    ],
-    [
-        "version",
-        ("GET /teddy/bear.html HTTP/1.0\r\n" +
-        "Date: Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
-        "Server: Teddy Bear\r\n" +
-        "Host:  \r\n")
-    ],
-    [
-        "host",
-        ("GET /teddy/bear.html HTTP/1.1\r\n" +
-        "Date: Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
-        "Server: Teddy Bear\r\n")
-    ],
-    [
-        "format",
-        ("GET /teddy/bear.html HTTP/1.1\r\n" +
-        "Date : Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
-        "Server: Teddy Bear\r\n" +
-        "Host:  \r\n")
-    ],
-]
-
-
-REQUESTS_RESPONSES = [
-    [
-        "OK",
-        ("GET /teddy/bear.html HTTP/1.1\r\n" +
-        "Date: Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
-        "Server: Teddy Bear\r\n" +
-        "Host:  \r\n"),
-        ("HTTP/1.1 200 OK\r\n" +
-        "Date: Mon, 23 May 2005 22:38:34 GMT\r\n" +
-        "Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n" +
-        "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n" +
-        "Etag: '3f80f-1b6-3e1cb03b'\r\n" +
-        "Accept-Ranges:  none\r\n" +
-        "Content-Length: 438\r\n" +
-        "Connection: close\r\n" +
-        "Content-Type: text/html; charset=UTF-8\r\n" +
-        "\r\n" +
-        "<438 bytes of content>")
-    ],
-    [
-        "method",
-        ("PUT /teddy/bear.html HTTP/1.1\r\n" +
-        "Date: Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
-        "Server: Teddy Bear\r\n" +
-        "Host:  \r\n"),
-        ("HTTP/1.1 405 Method Not Allowed\r\n" +
-        "Date: Mon, 23 May 2005 22:38:34 GMT\r\n" +
-        "Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n" +
-        "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n" +
-        "Etag: '3f80f-1b6-3e1cb03b'\r\n" +
-        "Accept-Ranges:  none\r\n" +
-        "Content-Length: 438\r\n" +
-        "Connection: close\r\n" +
-        "Content-Type: text/html; charset=UTF-8\r\n" +
-        "\r\n" +
-        "<438 bytes of content>")
-    ],
-    [
-        "version",
-        ("GET /teddy/bear.html HTTP/1.0\r\n" +
-        "Date: Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
-        "Server: Teddy Bear\r\n" +
-        "Host:  \r\n"),
-        ("HTTP/1.1 403 Forbidden\r\n" +
-        "Date: Mon, 23 May 2005 22:38:34 GMT\r\n" +
-        "Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n" +
-        "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n" +
-        "Etag: '3f80f-1b6-3e1cb03b'\r\n" +
-        "Accept-Ranges:  none\r\n" +
-        "Content-Length: 438\r\n" +
-        "Connection: close\r\n" +
-        "Content-Type: text/html; charset=UTF-8\r\n" +
-        "\r\n" +
-        "<438 bytes of content>")
-    ],
-    [
-        "host",
-        ("GET /teddy/bear.html HTTP/1.1\r\n" +
-        "Date: Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
-        "Server: Teddy Bear\r\n"),
-        ("HTTP/1.1 417 Expectation Failed\r\n" +
-        "Date: Mon, 23 May 2005 22:38:34 GMT\r\n" +
-        "Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n" +
-        "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n" +
-        "Etag: '3f80f-1b6-3e1cb03b'\r\n" +
-        "Accept-Ranges:  none\r\n" +
-        "Content-Length: 438\r\n" +
-        "Connection: close\r\n" +
-        "Content-Type: text/html; charset=UTF-8\r\n" +
-        "\r\n" +
-        "<438 bytes of content>")
-    ],
-    [
-        "format",
-        ("GET /teddy/bear.html HTTP/1.1\r\n" +
-        "Date : Mon, 27 Jul 1884 12:28:53 GMT\r\n" +
-        "Server: Teddy Bear\r\n" +
-        "Host:  \r\n"),
-        ("HTTP/1.1 400 Bad Request\r\n" +
-        "Date: Mon, 23 May 2005 22:38:34 GMT\r\n" +
-        "Server: Apache/1.3.3.7 (Unix) (Red-Hat/Linux)\r\n" +
-        "Last-Modified: Wed, 08 Jan 2003 23:11:55 GMT\r\n" +
-        "Etag: '3f80f-1b6-3e1cb03b'\r\n" +
-        "Accept-Ranges:  none\r\n" +
-        "Content-Length: 438\r\n" +
-        "Connection: close\r\n" +
-        "Content-Type: text/html; charset=UTF-8\r\n" +
-        "\r\n" +
-        "<438 bytes of content>")
-    ],
-]
-
-
-@pytest.mark.parametrize("status, req", REQUESTS)
-def test_method_validation(status, req):
-    """Test to see if GET requests are valid and any other type of requests are invalid."""
-    from server import method_validation
-    valid = method_validation(req)
-    if status == "method":
-        valid = not valid
-    assert valid
-
-
-@pytest.mark.parametrize("status, req", REQUESTS)
-def test_version_validation(status, req):
-    """Test to see if GET requests are valid and any other type of requests are invalid."""
-    from server import version_validation
-    valid = version_validation(req)
-    if status == "version":
-        valid = not valid
-    assert valid
-
-
-@pytest.mark.parametrize("status, req", REQUESTS)
-def test_host_validation(status, req):
-    """Test to see if GET requests are valid and any other type of requests are invalid."""
-    from server import host_validation
-    valid = host_validation(req)
-    if status == "host":
-        valid = not valid
-    assert valid
-
-
-@pytest.mark.parametrize("status, req", REQUESTS)
-def test_format_validation(status, req):
-    """Test to see if GET requests are valid and any other type of requests are invalid."""
-    from server import format_validation
-    valid = format_validation(req)
-    if status == "format":
-        valid = not valid
-    assert valid
-
-
-@pytest.mark.parametrize("request, response", REQUESTS_RESPONSES)
-def test_client_requests_responses(request, response):
-    """Test client requests to server return correct responses."""
+def test_response_ok():
+    """Test to see if valid client request will return a 200 OK message."""
     from client import client
-    assert client(request) == response
+    response = ("HTTP/1.1 200 OK\r\n" +
+        "Server: Teddy Bear\r\n" +
+        "Content-Type: text/html\r\n" +
+        "Connection: Closed\r\n\r\n" +
+        "<html>\r\n" +
+        "<body>\r\n" +
+        "<h1>Hello, World!</h1>\r\n" +
+        "</body>\r\n" +
+        "</html>")
+    assert client("GET the bl®rg woot") == response
+
+
+def test_response_failed():
+    """Test to see if invalid client request will return a 500 Error message."""
+    from client import client
+    response = ("HTTP/1.1 500 Internal Server Error\r\n" +
+        "Server: Teddy Bear\r\n" +
+        "Content-Type: text/html\r\n" +
+        "Connection: Closed\r\n\r\n" +
+        "<html>\r\n" +
+        "<body>\r\n" +
+        "<h1>Internal Server Error</h1>\r\n" +
+        "</body>\r\n" +
+        "</html>")
+    assert client("Get the bl®rg woot") == response
